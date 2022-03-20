e5adaf60390ab9bf97e0c8e0d8d205ce576bb30a
EduCastroo <educastro._@hotmail.com>
2022-03-20 20:35:51 -0300

    ex 46

--- Exercicio46/Program.cs
+++ Exercicio46/Program.cs
@@ -1,14 +1,37 @@
 ﻿// 46) Crie um programa que calcule e mostre na tela o resultado da soma entre 6 +8 + 10 + 12 + 14 + ... +98 + 100.
 
 
-int num = 1, soma = 0;
+int soma = 0;
+
+for (int numero = 6; numero <=100; numero++)
+{
+    //verifica se é par
+    if (numero % 2 == 0)
+    {
+        soma = soma + numero;
+        
+    }
+
+}
+Console.WriteLine(soma);
+
+
+
+
+
+
+
+
+
+/*int num = 6, soma = 0;
 
 while (num <= 100)
 {
-    if (num % 2 == 0)
+    if (num % 2 ==0)
     {
-        soma++;
+        Console.WriteLine(soma + num);
+        num++;
     }
 }
 
-
+Console.WriteLine(soma);*/
